1cb894168939cad19658bb2e88b8c71114e87fd4
amitranjan352 <100403569+amitranjan352@users.noreply.github.com>
2024-02-07 23:20:28 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,179 @@
+# # # i=1
+# # # l=2
+# # # while i<11:
+# # #     print(i*l,end=" ")
+# # #     i+=1
+# # n=int(input("enter a row:"))
+# # for i in range(0,n):
+# #     for j in range(0,i+1):
+# #         print("*",end="")
+# #     print()
+
+# # n = int(input("Enter the number of rows"))  
+
+# # for i in range(0, n):   
+# #         for j in range(0, i + 1):  
+          
+# #             print("* ", end="")       
+  
+        
+# #         print() 
+
+
+# # s="amitkumar" 
+# # print(s[::-1])
+
+
+# l1=["amit","aditya",56,98]
+# print("\n next line list:")
+# l1.append("cb")
+# print(l1)
+
+# f=open("text","r")
+# print(f.readline())
+# print(f.readline())
+
+# import os
+# print(os.getcwd())
+# f1=open("text","a")
+# print(f1.write("\ni am a funny people"))
+
+# a=[[2,4,6,7],[4,6,3,5],[3,4,5,6,],[8,9,7,6]]
+# for i in range(0,4):
+#     print(a[i][3],end=" ")
+
+# b=["amit","bhushan","ravi","sonu"]
+# for i in enumerate(b):
+#     print(i)
+
+
+# WAP to check weather num-1 and num-2 which is greater 
+
+
+# n1=int(input("enter a  first number"))
+# n2=int(input("enter a second number"))
+# if n1>n2:
+#     print(" first number:",n1)
+# else:
+#     print("second number:",n2)
+
+# # WAP to check given string is startswith vowels or not.
+
+# a="amit" 
+
+
+# for i in a:
+#     if i[0] in"aeiou":
+#         print("vowel")
+#     else:
+#         print("not vowel")
+       
+        
+    
+
+
+# #Write a Python program to read an entire text file.
+# with open("text","r") as file:
+#     print(file.read())
+
+# #Write a Python program to read first n lines of a file.
+# with open("text","r")as file:
+#     print(file.readline())
+
+
+# a="amit"
+# if a.startswith("aeiou"):
+#         print("amit is vowel")
+# else:
+#         print("it is constant")
+# my_word="aumm"
+# if my_word[0] in ('a','e','i','o','u'):
+#    print(' Yes, the first letter is vowel ')
+# else:
+#    print(' No, the first letter is not vowel ')
+
+
+# #WAP to check given number is divisible by 5 and 3.
+
+# a=int(input("enter a number :"))
+# if a%5==0 and a%3==0:
+# #    print("true")
+# # else:
+# #    print("false")
+
+# # #WAP to check given number is even or odd.
+
+# # x=int(input("enter a number::"))
+# # if x%2==0:
+# #    print("given number is even")
+
+# # else:
+# #    print("given number is odd")
+
+
+# # WAP to check first digit of given number is even or odd.
+
+
+# email= input("enter a right email: ")
+# if "@" in email:
+#     password=input(" enter a password:")
+#     if email=="amitranjan311098@gmail.com"or password=="1234":
+
+#         print("welcome to my dashboard")
+#     elif email=="amitranjan311098@gmail.com" or password !="1234":
+#         print("one again")
+#         password=input("again password store")
+#         if password=="1234":
+#             print("password correct")
+#         else:
+#             print("still incorrect")
+# else:
+#     print("going to wrong method")
+
+#  a=int(input ("enter a number:"))
+
+
+#  while a<10:
+#     a=a//10
+#  if a % 2==0:
+#      print("even")
+#  else:
+#      print("odd")
+
+#  WAP to convert given string into uppercase.
+
+#  y="python is general purpose programming language"
+# print(y.upper())
+
+# #WAP to convert upper to lower or lower to upper.
+# a="AMIT"
+# print(a.swapcase())
+
+# x=int(input("enter a first  number"))
+# y=int(input("enter a second number"))
+# print(x+y)
+
+# # WAP to check number of elements present in list, if it is even   
+# #      print Good morning else print good evening.
+
+# a=[24,56,67,98]
+# for i in a:
+#     if i%2==0:
+#         print("good morning")
+#     else:
+#         print("good evening")
+
+
+# b=[34,87,98]
+# if b%2==0:
+#     print("good morning")
+# else:
+#     print("good evening")
+
+
+
+
+   
+
+   
+    
